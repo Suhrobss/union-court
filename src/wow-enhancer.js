766719ba45fbbb6388b02968220b9fdaf10b6d31
Suhrobss <suhrobworks@gmail.com>
2026-08-19 16:12:41 +0300

    Remove retired legacy enhancer

--- src/wow-enhancer.js
+++
@@ -1,3 +0,0 @@
-// Legacy enhancer retired.
-// The current application is implemented in main.jsx and gameData.js.
-// Intentionally contains no source-case identifiers or reconstructed source documents.
